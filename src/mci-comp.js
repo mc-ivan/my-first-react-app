66541a553c16bca4330f2d2da74df435341d9861
Ivan Mamani <mamaniconivan@gmail.com>
2021-10-10 16:41:31 -0400

    REACT-01 Add items functionality

--- src/mci-comp.js
+++ src/mci-comp.js
@@ -5,21 +5,44 @@ class MCI_COMP extends React.Component {
         super(props);
 
         this.state = {
-            name: ""
+            name: "",
+            items: []
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange (e) {
         this.setState({name: e.target.value})
     }
 
+    handleSubmit (e) {
+        e.preventDefault()
+        if (this.state.name.length === 0) return
+
+        const newItem = {
+            name: this.state.name,
+            id: Date.now()
+        }
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            name: ""
+        }))
+
+
+    }
+
     render() {
         return (
             <div>
                 <h1>MCI APPLICATION</h1>
-                <form>
+                <ul>
+                    {this.state.items.map(item => (
+                        <li key={item.id}>{item.name}</li>
+                    ))}
+                </ul>
+                <form onSubmit={this.handleSubmit}>
                     <label htmlFor="name">Name</label><br/>
                     <label>{this.state.name}</label><br/>
                     <input id="name" onChange={this.handleChange} value={this.state.name}/>
